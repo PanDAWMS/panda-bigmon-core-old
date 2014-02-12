ccba6120165f9641109120a6b6d2c256a64c680f
jschovan <jschovan@gmail.com>
2014-02-12 22:15:28 +0000

    panda-bigmon -- bigpandamon-core common settings

--- core/common/settings/base.py
+++ core/common/settings/base.py
@@ -1,54 +1,51 @@
-"""
-Django settings for bigpandamon project.
+# -*- coding: utf-8 -*-
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+# Django base settings for common core.
 
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
+from os.path import dirname, join
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+import core
+import lsst
+#import atlas
 
-from localsettings import defaultDatabase, MY_SECRET_KEY
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = MY_SECRET_KEY
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = [
-    ### cern.ch
-    '.cern.ch',  # Allow domain and subdomains
-    '.cern.ch.',  # Also allow FQDN and subdomains
-    ### pandawms.org
-    '.pandawms.org',  # Allow domain and subdomains
-    '.pandawms.org.',  # Also allow FQDN and subdomains
-]
-
-# Application definition
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',  #pip install djangorestframework, version 2.3.10
-    'django_datatables_view',  #pip install django-datatables-view, version 1.6
-#     'bigpandamon',
-    'bigpandamon',
+ADMINS = (
+    ('Jaroslava Schovancova', 'jschovan@gmail.com'),
 )
+MANAGERS = ADMINS
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Site ID
+SITE_ID = 1
+
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -69,273 +66,45 @@ MIDDLEWARE_CLASSES = (
 #    # Uncomment the next line for simple clickjacking protection:
 #    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
-### added
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.RemoteUserBackend',  # for APIs: htcondorapi
-)
-### END added
 
-ROOT_URLCONF = 'bigpandamon.urls'
-
-WSGI_APPLICATION = 'bigpandamon.wsgi.application'
+ROOT_URLCONF = 'core.common.urls'
 
 ### added
 TEMPLATE_DIRS = (
-        # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-        # Always use forward slashes, even on Windows.
-        # Don't forget to use absolute paths, not relative paths.
-        "/data/bigpandamon/bigpandamon/templates",
-)
-### END added
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': '',                      # Or path to database file if using sqlite3.
-#        'USER': '',                      # Not used with sqlite3.
-#        'PASSWORD': '',                  # Not used with sqlite3.
-#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#    }
-    'default': defaultDatabase
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOG_ROOT = '/data/bigpandamon/bigpandamon/logs/'
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-#    'disable_existing_loggers': True,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level':'DEBUG',
-            'class':'django.utils.log.NullHandler',
-        },
-        'logfile-bigpandamon': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': LOG_ROOT + "/logfile.bigpandamon",
-            'maxBytes': 50000,
-            'backupCount': 2,
-            'formatter': 'verbose',
-        },
-        'logfile-django': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': LOG_ROOT + "/logfile.django",
-            'maxBytes': 50000,
-            'backupCount': 2,
-            'formatter': 'verbose',
-        },
-        'logfile-viewdatatables': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': LOG_ROOT + "/logfile.viewdatatables",
-            'maxBytes': 50000,
-            'backupCount': 2,
-            'formatter': 'verbose',
-        },
-        'logfile-rest': {
-            'level':'DEBUG',
-            'class':'logging.handlers.RotatingFileHandler',
-            'filename': LOG_ROOT + "/logfile.rest",
-            'maxBytes': 50000,
-            'backupCount': 2,
-            'formatter': 'verbose',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-#            'class': 'django.utils.log.AdminEmailHandler'
-            'class':'logging.StreamHandler',
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-#            'level': 'ERROR',
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django': {
-            'handlers':['logfile-django'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'django_datatables_view': {
-            'handlers':['logfile-viewdatatables'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'rest_framework': {
-            'handlers':['logfile-rest'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'bigpandamon': {
-            'handlers': ['logfile-bigpandamon'],
-            'level': 'DEBUG',
-        },
-    },
-    'formatters': {
-        'verbose': {
-#            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-            'format': '%(asctime)s %(module)s %(name)-12s:%(lineno)d %(levelname)-5s %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(name)-12s:%(lineno)d %(message)s'
-        },
-    },
-    'logfile': {
-        'level':'DEBUG',
-        'class':'logging.handlers.RotatingFileHandler',
-        'filename': LOG_ROOT + "/logfile",
-        'maxBytes': 10000000,
-        'backupCount': 5,
-        'formatter': 'verbose',
-    },
-}
-
-
-### added
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/data/bigpandamon/bigpandamon/media/'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/data/bigpandamon/bigpandamon/static/'
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-#STATIC_URL = '/static/'
-STATIC_URL_BASE = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    join(dirname(core.common.__file__), 'templates'),
 )
 
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+# installed apps
+INSTALLED_APPS = (
+    ### Django framework
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    ### Django plugins
+    'rest_framework',  #pip install djangorestframework, version 2.3.10
+    'django_datatables_view',  #pip install django-datatables-view, version 1.6
+    ### BigPanDAmon core
+    'core.common',
+    'core.table',
+#    'core.graphics', #NOT-IMPLEMENTED
+    'core.pandajob',
+    'core.resource',
+    'core.htcondor',
+#    'core.task', #NOT-IMPLEMENTED
 )
 
-### END added
+VERSION = core.common.__versionstr__
 
-
-### VIRTUALENV
-VIRTUALENV_PATH = '/data/virtualenv/django1.6.1__python2.6.6'
-
-### WSGI
-WSGI_PATH = VIRTUALENV_PATH + '/bigpandamon'
-
-
-### topology
-SOURCE_SCHEDCONFIG = {
-    'lsst': 'http://atlas-agis-api.cern.ch/request/pandaqueue/query/list/?json&preset=schedconf.all&site_state=ANY&vo_name=lsst',
+VERSIONS = {
+    'core': core.__versionstr__,
+    'lsst': lsst.__versionstr__,
+#    'atlas': atlas.__versionstr__,
 }
-
-### jobs by ProdUserName
-CUSTOM_DB_FIELDS = {
-    'jobListByProdUser': {
-        'jobparam': ['jobStatus', 'cpuConsumptionTime', 'creationTime', \
-                     'startTime', 'endTime', 'modificationHost', 'computingSite', \
-                     'prodUserName'], \
-        'configurable': ['jobparam', 'prodUserName', 'days']
-    }
-}
-
-### URL_PATH_PREFIX for multi-developer apache/wsgi instance
-### on EC2: URL_PATH_PREFIX = '/bigpandamon' or URL_PATH_PREFIX = '/developersprefix'
-URL_PATH_PREFIX = '/bigpandamon'
-### on localhost:8000: URL_PATH_PREFIX = '/.'
-#URL_PATH_PREFIX = ''
-MEDIA_URL = URL_PATH_PREFIX + MEDIA_URL
-STATIC_URL = str(URL_PATH_PREFIX) + str(STATIC_URL_BASE)
-
-#print 'STATIC_URL=', STATIC_URL
-
-ENV = {
-    ### Application name
-#    'APP_NAME': "ProdSys2", \
-    'APP_NAME': "BigPanDA", \
-    ### Page title default
-    'PAGE_TITLE': "BigPanDA Monitor", \
-    ### Menu item separator
-    'SEPARATOR_MENU_ITEM': "&nbsp;&nbsp;&nbsp;", \
-    ### Navigation chain item separator
-    'SEPARATOR_NAVIGATION_ITEM': "&nbsp;&#187;&nbsp;" , \
-}
-
-FILTER_UI_ENV = {
-    ### default number of days of shown jobs active in last N days
-    'DAYS': 1, \
-    ### default number of hours of shown jobs active in last N hours
-    'HOURS': 2, \
-    ### wildcard for string pattern in filter form
-    'WILDCARDS': ['*'], \
-    ### wildcard for integer interval in filter form
-    'INTERVALWILDCARDS': [':'], \
-    ###
-    'EXPAND_BUTTON': { "mDataProp": None, "sTitle": "Details", \
-                       "sClass": "control center", "bVisible": True, \
-                       "bSortable": False, \
-                       "sDefaultContent": '<img src="' + STATIC_URL + \
-                                '/images/details_open.png' + '">' \
-            }, \
-}
-
-#TODO:
-#CSRF_FAILURE_VIEW
-#404.html, 500.html
-
 
