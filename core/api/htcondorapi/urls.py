7340f4f0235f6cb934d1dc8278736ea874557624
jschovan <jschovan@gmail.com>
2014-03-12 19:48:26 +0000

    panda-htcondorapi -- improving url structure for the HTCondor API. Adding a new test script.

--- core/api/htcondorapi/urls.py
+++ core/api/htcondorapi/urls.py
@@ -10,32 +10,40 @@ from django.contrib import admin
 import views as htcondorapi_views
 
 #### Actions of HTCondor API
-htcondorapi_addjob = htcondorapi_views.HTCondorJobsViewSet.as_view(
+#htcondorapi_addjob = htcondorapi_views.HTCondorJobsViewSet.as_view(
+#{
+#    'get': 'list',
+#    'post': 'addHTCondorJobs',
+#})
+#htcondorapi_updatejob = htcondorapi_views.HTCondorJobsViewSet.as_view(
+#{
+#    'get': 'list',
+#    'post': 'updateHTCondorJobs',
+#})
+#htcondorapi_removejob = htcondorapi_views.HTCondorJobsViewSet.as_view(
+#{
+#    'get': 'list',
+#    'post': 'removeHTCondorJobs',
+#})
+#htcondorapi_listjob = htcondorapi_views.HTCondorJobsViewSet.as_view(
+#{
+#    'get': 'listForDataTables',
+#})
+htcondorapi_jobs_bulk = htcondorapi_views.HTCondorJobsViewSet.as_view(
 {
-    'get': 'list',
     'post': 'addHTCondorJobs',
-})
-htcondorapi_updatejob = htcondorapi_views.HTCondorJobsViewSet.as_view(
-{
     'get': 'list',
-    'post': 'updateHTCondorJobs',
-})
-htcondorapi_removejob = htcondorapi_views.HTCondorJobsViewSet.as_view(
-{
-    'get': 'list',
-    'post': 'removeHTCondorJobs',
-})
-htcondorapi_listjob = htcondorapi_views.HTCondorJobsViewSet.as_view(
-{
-    'get': 'listForDataTables',
+    'put': 'updateHTCondorJobs',
+    'delete': 'removeHTCondorJobs',
 })
 
 urlpatterns = patterns('',
     ### REST framework API
-    url(r'^api-auth/htcondorapi/addjob/$', htcondorapi_addjob, name='htcondorapi-add-job'),
-    url(r'^api-auth/htcondorapi/updatejob/$', htcondorapi_updatejob, name='htcondorapi-update-job'),
-    url(r'^api-auth/htcondorapi/removejob/$', htcondorapi_removejob, name='htcondorapi-remove-job'),
-    url(r'^api/listall/htcondor/$', htcondorapi_listjob, name='api-datatables-htcondor-dict'),
+#    url(r'^api-auth/htcondorapi/addjob/$', htcondorapi_addjob, name='htcondorapi-add-job'),
+#    url(r'^api-auth/htcondorapi/updatejob/$', htcondorapi_updatejob, name='htcondorapi-update-job'),
+#    url(r'^api-auth/htcondorapi/removejob/$', htcondorapi_removejob, name='htcondorapi-remove-job'),
+#    url(r'^api/listall/htcondor/$', htcondorapi_listjob, name='api-datatables-htcondor-dict'),
+    url(r'^jobs/$', htcondorapi_jobs_bulk, name='htcondorapi-jobs-bulk'),
 )
 
 
