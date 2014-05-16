df15b372252c921a6ef6bfd8ce33fb86559049eb
wenaus <wenaus@gmail.com>
2014-05-16 00:55:03 +0000

    First commit

--- lsst/urls.py
+++ lsst/urls.py
@@ -1,11 +1,29 @@
+from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.common.urls import *
+#from core.common.urls import *
 
 import lsst.settings
 
-urlpatterns = patterns('',)
-urlpatterns += common_patterns
+import lsst.views as lsstmon_views
+
+urlpatterns = patterns('',
+    url(r'^$', lsstmon_views.mainPage),
+    url(r'^lsst/$', lsstmon_views.mainPage),
+    url(r'^lsst/jobs/$', lsstmon_views.jobList),
+    url(r'^lsst/jobs/(.*)/$', lsstmon_views.jobList),
+    url(r'^lsst/jobs/(.*)/(.*)/$', lsstmon_views.jobList),
+    url(r'^lsst/job$', lsstmon_views.jobInfo),
+    url(r'^lsst/job/(.*)/$', lsstmon_views.jobInfo),
+    url(r'^lsst/job/(.*)/(.*)/$', lsstmon_views.jobInfo),
+    url(r'^lsst/users/$', lsstmon_views.userList),
+    url(r'^lsst/user/(?P<user>.*)/$', lsstmon_views.userInfo),
+    url(r'^lsst/sites/$', lsstmon_views.siteList),
+    url(r'^lsst/site/(?P<site>.*)/$', lsstmon_views.siteInfo),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+#urlpatterns += common_patterns
 #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
