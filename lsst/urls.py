a524dd06553163f0c17a9b867449d2ca64364829
jschovan <jschovan@gmail.com>
2014-05-24 00:17:25 +0000

    Adding robots.txt to bigpandamon-lsst.

--- lsst/urls.py
+++ lsst/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 #from core.common.urls import *
 
