c612c57933eb8040b87d35ba5bfc652ec7d92f0b
wenaus <wenaus@gmail.com>
2014-06-01 13:33:18 +0000

    update user, job views

--- lsst/views.py
+++ lsst/views.py
@@ -450,6 +450,15 @@ def jobList(request, mode=None, param=None):
         jobtype = 'analysis'
     elif '/production' in request.path:
         jobtype = 'production'
+    tfirst = timezone.now()
+    tlast = timezone.now() - timedelta(hours=2400)
+    plow = 1000000
+    phigh = -1000000
+    for job in jobs:
+        if job['modificationtime'] > tlast: tlast = job['modificationtime']
+        if job['modificationtime'] < tfirst: tfirst = job['modificationtime']
+        if job['currentpriority'] > phigh: phigh = job['currentpriority']
+        if job['currentpriority'] < plow: plow = job['currentpriority']
     if request.META.get('CONTENT_TYPE', 'text/plain') == 'text/plain':
         sumd = jobSummaryDict(request, jobs)
         xurl = extensibleURL(request)
@@ -465,6 +474,10 @@ def jobList(request, mode=None, param=None):
             'xurl' : xurl,
             'droplist' : droplist,
             'ndrops' : len(droplist),
+            'tfirst' : tfirst,
+            'tlast' : tlast,
+            'plow' : plow,
+            'phigh' : phigh,
         }
         data.update(getContextVariables(request))
         return render_to_response('jobList.html', data, RequestContext(request))
