3d0f2026da909b8459172475a74c7a45bafba4d8
jschovan <jschovan@gmail.com>
2014-06-02 00:11:24 +0000

    Refactor - core.pandajob.urls and lsst.urls.

--- lsst/urls.py
+++ lsst/urls.py
@@ -3,13 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
-#from core.common.urls import *
+#import lsst.settings
+from django.conf import settings
 
-import lsst.settings
-
-import lsst.views as lsstmon_views
-import core.pandajob.views_support as core_lsstmon_support_views
-import core.pandajob.views as core_lsstmon_views
+#import lsst.views as lsstmon_views
+#import core.pandajob.views_support as core_lsstmon_support_views
+#import core.pandajob.views as core_lsstmon_views
 
 urlpatterns = patterns('',
 #    url(r'^$', lsstmon_views.mainPage, name='mainPage'),
@@ -31,30 +30,40 @@ urlpatterns = patterns('',
 #    url(r'^dash/analysis/$', lsstmon_views.dashAnalysis, name='dashAnalysis'),
 #    url(r'^dash/production/$', lsstmon_views.dashProduction, name='dashProduction'),
 
-    url(r'^$', core_lsstmon_views.mainPage, name='mainPage'),
-    url(r'^jobs/$', core_lsstmon_views.jobList, name='jobList'),
-    url(r'^jobs/(.*)/$', core_lsstmon_views.jobList, name='jobList'),
-    url(r'^jobs/(.*)/(.*)/$', core_lsstmon_views.jobList, name='jobList'),
-    url(r'^job$', core_lsstmon_views.jobInfo, name='jobInfo'),
-    url(r'^job/(.*)/$', core_lsstmon_views.jobInfo, name='jobInfo'),
-    url(r'^job/(.*)/(.*)/$', core_lsstmon_views.jobInfo, name='jobInfo'),
-    url(r'^users/$', core_lsstmon_views.userList, name='userList'),
-    url(r'^user/(?P<user>.*)/$', core_lsstmon_views.userInfo, name='userInfo'),
-    url(r'^sites/$', core_lsstmon_views.siteList, name='siteList'),
-    url(r'^site/(?P<site>.*)/$', core_lsstmon_views.siteInfo, name='siteInfo'),
-    url(r'^tasks/$', core_lsstmon_views.taskList, name='taskList'),
-    url(r'^task$', core_lsstmon_views.taskInfo, name='taskInfo'),
-    url(r'^task/$', core_lsstmon_views.taskInfo, name='taskInfo'),
-    url(r'^task/(?P<jeditaskid>.*)/$', core_lsstmon_views.taskInfo, name='taskInfo'),
-    url(r'^dash/$', core_lsstmon_views.dashboard, name='dashboard'),
-    url(r'^dash/analysis/$', core_lsstmon_views.dashAnalysis, name='dashAnalysis'),
-    url(r'^dash/production/$', core_lsstmon_views.dashProduction, name='dashProduction'),
-
-###    url(r'^job/', include('core.pandajob.urls')),
-
+#    url(r'^$', core_lsstmon_views.mainPage, name='mainPage'),
+#    url(r'^jobs/$', core_lsstmon_views.jobList, name='jobList'),
+#    url(r'^jobs/(.*)/$', core_lsstmon_views.jobList, name='jobList'),
+#    url(r'^jobs/(.*)/(.*)/$', core_lsstmon_views.jobList, name='jobList'),
+#    url(r'^job$', core_lsstmon_views.jobInfo, name='jobInfo'),
+#    url(r'^job/(.*)/$', core_lsstmon_views.jobInfo, name='jobInfo'),
+#    url(r'^job/(.*)/(.*)/$', core_lsstmon_views.jobInfo, name='jobInfo'),
+#    url(r'^users/$', core_lsstmon_views.userList, name='userList'),
+#    url(r'^user/(?P<user>.*)/$', core_lsstmon_views.userInfo, name='userInfo'),
+#    url(r'^sites/$', core_lsstmon_views.siteList, name='siteList'),
+#    url(r'^site/(?P<site>.*)/$', core_lsstmon_views.siteInfo, name='siteInfo'),
+#    url(r'^tasks/$', core_lsstmon_views.taskList, name='taskList'),
+#    url(r'^task$', core_lsstmon_views.taskInfo, name='taskInfo'),
+#    url(r'^task/$', core_lsstmon_views.taskInfo, name='taskInfo'),
+#    url(r'^task/(?P<jeditaskid>.*)/$', core_lsstmon_views.taskInfo, name='taskInfo'),
+#    url(r'^dash/$', core_lsstmon_views.dashboard, name='dashboard'),
+#    url(r'^dash/analysis/$', core_lsstmon_views.dashAnalysis, name='dashAnalysis'),
+#    url(r'^dash/production/$', core_lsstmon_views.dashProduction, name='dashProduction'),
     ### support views for LSST
-    url(r'^support/$', core_lsstmon_support_views.maxpandaid, name='supportRoot'),
-    url(r'^support/maxpandaid/$', core_lsstmon_support_views.maxpandaid, name='supportMaxpandaid'),
+#    url(r'^support/$', core_lsstmon_support_views.maxpandaid, name='supportRoot'),
+#    url(r'^support/maxpandaid/$', core_lsstmon_support_views.maxpandaid, name='supportMaxpandaid'),
+
+    url(r'^/', include('core.pandajob.urls_pandajob_mainpage')),
+    url(r'^jobs/', include('core.pandajob.urls_pandajob_jobs')),
+    url(r'^job/', include('core.pandajob.urls_pandajob_singlejob')),
+    url(r'^users/', include('core.pandajob.urls_pandajob_users')),
+    url(r'^user/', include('core.pandajob.urls_pandajob_singleuser')),
+    url(r'^sites/', include('core.pandajob.urls_pandajob_sites')),
+    url(r'^site/', include('core.pandajob.urls_pandajob_singlesite')),
+    url(r'^tasks/', include('core.pandajob.urls_pandajob_tasks')),
+    url(r'^task/', include('core.pandajob.urls_pandajob_singletask')),
+    url(r'^dash/', include('core.pandajob.urls_pandajob_dash')),
+    url(r'^support/', include('core.pandajob.urls_pandajob_support')),
+
 
     ### robots.txt
     url('^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
