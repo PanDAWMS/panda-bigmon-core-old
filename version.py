b1afaf6ea985d59d3dc49d01f23fb79d37966070
Jaroslava Schovancova <jschovan@gmail.com>
2014-08-06 15:51:48 -0400

    Version string fixing.

--- version.py
+++ version.py
@@ -1,5 +1,5 @@
 """ 
-    core.version
+    atlas.version 
 """
 import commands
 import ConfigParser
@@ -18,35 +18,50 @@ def get_version_base__release_type__provides():
     return (version_base, release_type, provides,)
 
 
-def get_svn_version():
+def get_git_version():
     """
-        get version of this svn commit
+        get version of this git commit
     """
     dir = os.path.dirname(os.path.realpath(__file__))
+    ### count cap of total number of commits in this repository
+    ncommits = 1
+    try:
+        ncommits = commands.getoutput('git shortlog | wc -l')
+    except:
+        pass
     ### get last revision ID, short version
     last_rev_id = ''
     try:
-        last_rev_id = commands.getoutput(' svnversion core/ ')
+        last_rev_id = commands.getoutput('git show -s --pretty=format:%h ')
     except:
         pass
-    return str('.dev-r' + last_rev_id)
+    ### get number of revisions on this branch
+    nrevs = 1
+    try:
+        nrevs = commands.getoutput('git reflog | wc -l')
+    except:
+        pass
+    if ncommits < 19000:
+        retstr = str('.dev-' + ncommits + '-' + last_rev_id + '-' + nrevs)
+    else:
+        retstr = str('.dev-' + nrevs + '-' + last_rev_id + '-' + ncommits + nrevs)
+    return retstr
 
 
 def get_version_provides():
-#    __version__ = get_svn_version()
-#    __provides__ = get_version_base__release_type__provides()
-#    return (__version__, __provides__,)
     isStable = False
     version_base, release_type, provides = get_version_base__release_type__provides()
     if release_type == 'stable':
         isStable = True
     __version__ = version_base
     if not isStable:
-        __version__ += get_svn_version()
+        __version__ += get_git_version()
     __provides__ = provides
     return (__version__, __provides__,)
 
 
+
 __version__, __provides__ = get_version_provides()
+
 
 
